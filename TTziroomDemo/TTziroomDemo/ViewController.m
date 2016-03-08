85942ec8b4b843654ef538de8296f9ecc5094773
TT_dashen <ltt143@126.com>
2016-03-08 11:44:45 +0800

    creat project

---
+++ TTziroomDemo/TTziroomDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TTziroomDemo
+//
+//  Created by 梁腾 on 16/3/8.
+//  Copyright © 2016年 TT. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
